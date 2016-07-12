4ad50029eb7e4e3934c6e6536bdf6397dffd7853
= <=>
2016-07-12 15:07:06 -0300

    email

---
+++ mail.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+try {
+$título = 'Matchpad email';
+
+// mensaje
+$mensaje = '
+<html>
+<head>
+  <title>Matchpad</title>
+</head>
+<body>
+    <p>Nombre:' . $_POST["name"] . '</p>
+    <p>Email:' . $_POST["email"] . '</p>
+                                  
+</body>
+</html>
+';
+
+$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$cabeceras .= 'From: ezequielcarrosalle@gmail.com' . "\r\n" .
+    'Reply-To: ezequielcarrosalle@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+echo json_encode(mail("ezequielcarrosalle@gmail.com", "matchpad", $mensaje,$cabeceras));
+} catch (Exception $e) {
+    echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+}
